f2cb165a4c20480aef6094abe808b7ac1ea0f0ac
alishercpp <alisher.abdimuminov.2005@gmail.com>
2024-11-01 11:40:59 +0500

    bug fix

--- layouts/auth.vue
+++ layouts/auth.vue
@@ -1,7 +1,15 @@
+<script setup lang="ts">
+import logo from "~/assets/images/logo.png";
+</script>
+
 <template>
     <div class="h-screen flex">
-        <div class="hidden md:block w-full bg-accent p-10">
-            <p class="text-2xl font-bold">Davlat ekologik ekspertiza markazi</p>
+        <div class="relative hidden md:block w-full bg-accent p-10" :style="{ backgroundImage: `url(${logo})`, backgroundSize: '50%', backgroundRepeat: 'no-repeat', backgroundPosition: 'center' }">
+             <div class="absolute text-center w-full left-0 bottom-20">
+                <p class="text-2xl font-bold w-full">
+                    <span class="block">Davlat ekologik ekspertiza markazi</span>
+                </p>
+            </div>
         </div>
         <div class="w-full flex items-center justify-center">
             <slot />
