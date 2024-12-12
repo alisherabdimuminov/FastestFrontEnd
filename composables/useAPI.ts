95573e69084860087e520760a658b55b6d75b324
alishercpp <alisher.abdimuminov.2005@gmail.com>
2024-12-12 11:06:02 +0500

    bug fix

--- composables/useAPI.ts
+++ composables/useAPI.ts
@@ -1,4 +1,4 @@
 export default function apify(path: string) {
     // return `http://localhost:8000/api/v1/${path}/`;
-    return `https://ecotest.pythonanywhere.com/api/v1/${path}/`;
+    return `https://fintest.pythonanywhere.com/api/v1/${path}/`;
 }
